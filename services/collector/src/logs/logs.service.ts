f529465b69a0ee3e49905f450cbb794495ad455f
Adriano Christian <adrianochristian3@gmail.com>
2025-04-08 18:54:18 -0300

    feat(collector): sending the log to Kafka

--- services/collector/src/logs/logs.service.ts
+++ services/collector/src/logs/logs.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateLogDto } from './dto/create-log.dto';
+import { Kafka, Producer } from 'kafkajs';
 
 @Injectable()
-export class LogsService {
+export class LogsService implements OnModuleInit {
+  private kafka = new Kafka({ brokers: [process.env.KAFKA_BROKER || 'localhost:9092'] });
+  private producer!: Producer;
+
+  async onModuleInit() {
+    this.producer = this.kafka.producer();
+    await this.producer.connect();
+  }
+
   async publish(log: CreateLogDto) {
-    console.log('Log recebido:', log);
-    // TODO: publicar no Kafka
+    await this.producer.send({
+      topic: 'logs',
+      messages: [{ value: JSON.stringify(log) }],
+    });
+    console.log('Publicado no Kafka:', log);
   }
 }
